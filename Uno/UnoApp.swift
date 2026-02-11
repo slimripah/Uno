1b2dbfd3958614eefc943cf89800ed8829404815
slimripah <slimripah@gmail.com>
2026-02-11 15:09:19 +0300

    Initial Commit

---
+++ Uno/UnoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnoApp.swift
+//  Uno
+//
+//  Created by Joe on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct UnoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
